3a5297b7000ad7a1681efdaf0a97b94e706bb841
gradlevv <j.sadatii@gmail.com>
2022-01-20 21:07:32 +0330

    add util class

---
+++ core/src/main/java/com/gradlevv/core/util/Extensions.kt
@@ -0,0 +1,33 @@
+package com.gradlevv.core.util
+
+import android.content.res.Resources
+import android.util.TypedValue
+import kotlin.math.ceil
+
+fun Int.dpf(): Float {
+    return this.dpf().toFloat()
+}
+
+fun Float.dpf(): Float {
+    return this.dpf().toFloat()
+}
+
+fun Int.dp(): Int {
+    return if (this == 0) {
+        0
+    } else ceil(Resources.getSystem().displayMetrics.density * this).toDouble().toInt()
+}
+
+fun Float.dp(): Int {
+    return if (this == 0f) {
+        0
+    } else ceil(Resources.getSystem().displayMetrics.density * this).toDouble().toInt()
+}
+
+fun Int.sp(): Float {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_SP,
+        this.toFloat(),
+        Resources.getSystem().displayMetrics
+    )
+}
