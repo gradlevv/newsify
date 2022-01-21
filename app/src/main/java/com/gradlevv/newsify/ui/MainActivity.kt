e729e04269792037bed9dca995e83af18365d605
gradlevv <j.sadatii@gmail.com>
2022-01-21 16:14:57 +0330

    init navigation component

--- app/src/main/java/com/gradlevv/newsify/ui/MainActivity.kt
+++ app/src/main/java/com/gradlevv/newsify/ui/MainActivity.kt
@@ -2,14 +2,42 @@ package com.gradlevv.newsify.ui
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.gradlevv.newsify.R
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var root: FrameLayout
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    private lateinit var navController: NavController
+    private lateinit var navGraph: NavGraph
+    private lateinit var navHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_main)
+        setUpView()
+    }
+
+    private fun setUpView() {
+
+        root = findViewById(R.id.root)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+
+        val graphInflater = navHostFragment.navController.navInflater
+        navGraph = graphInflater.inflate(R.navigation.navigation)
+        navController = navHostFragment.navController
+
+
     }
 
 }
