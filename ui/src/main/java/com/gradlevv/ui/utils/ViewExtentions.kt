3dacd440bf42537f9d566964d6ca5d43701bfa18
gradlevv <j.sadatii@gmail.com>
2022-03-28 16:18:58 +0430

    add glide dependency, create loadImageView extension function

--- ui/src/main/java/com/gradlevv/ui/utils/ViewExtentions.kt
+++ ui/src/main/java/com/gradlevv/ui/utils/ViewExtentions.kt
@@ -5,11 +5,18 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
+import com.bumptech.glide.Glide
 import com.gradlevv.core.util.getCompatDrawable
 
 
 fun ImageView.setCompatDrawable(@DrawableRes id: Int) {
     setImageDrawable(context.getCompatDrawable(id))
+}
+
+fun ImageView.loadImage(url: String?){
+    Glide.with(this)
+        .load(url)
+        .into(this)
 }
 
 fun View.closeKeyboard() {
