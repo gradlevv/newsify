6b11900b3757e937efeaf8d3279fa200b7e16380
gradlevv <j.sadatii@gmail.com>
2022-04-23 18:00:58 +0430

    refactor hard coded dispatcher with injected dispatcher

--- news/list/src/main/java/com/gradlevv/list/data/source/NewsListRepositoryImpl.kt
+++ news/list/src/main/java/com/gradlevv/list/data/source/NewsListRepositoryImpl.kt
@@ -3,20 +3,23 @@ package com.gradlevv.list.data.source
 import com.gradlevv.core.data.model.Resource
 import com.gradlevv.core.data.model.mapTo
 import com.gradlevv.core.data.network.ResponseHandler
+import com.gradlevv.core.util.IoDispatcher
 import com.gradlevv.list.data.TopHeadLinesMapper
 import com.gradlevv.list.domain.NewsListRepository
 import com.gradlevv.list.domain.TopHeadLinesItem
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class NewsListRepositoryImpl @Inject constructor(
     private val service: NewsListService,
-    private val topHeadLinesMapper: TopHeadLinesMapper
+    private val topHeadLinesMapper: TopHeadLinesMapper,
+    @IoDispatcher
+    private val dispatcher: CoroutineDispatcher
 ) : ResponseHandler(), NewsListRepository {
 
     override suspend fun getTopHeadLines(): Resource<List<TopHeadLinesItem>> {
-        return withContext(Dispatchers.IO) {
+        return withContext(dispatcher) {
             return@withContext getResource { service.getTopHeadLines() }
                 .mapTo(mapper = topHeadLinesMapper)
         }
