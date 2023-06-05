4e724abee73a94174bedd359e9966049788880f1
gradlevv <j.sadatii@gmail.com>
2023-06-05 19:11:46 +0330

    add getResultFlow method

--- core/src/main/java/com/gradlevv/core/data/network/ResponseHandler.kt
+++ core/src/main/java/com/gradlevv/core/data/network/ResponseHandler.kt
@@ -3,6 +3,10 @@ package com.gradlevv.core.data.network
 import com.gradlevv.core.data.model.ApiError
 import com.gradlevv.core.data.model.ErrorHandler.errorParser
 import com.gradlevv.core.data.model.Result
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import retrofit2.Response
 import timber.log.Timber
 
@@ -29,4 +33,28 @@ abstract class ResponseHandler {
         }
     }
 
+    protected fun <T> getResultFlow(call: suspend () -> Response<T>): Flow<Result<T>> {
+        return flow {
+            try {
+                val response: Response<T> = call()
+
+                if (response.isSuccessful) {
+                    val rawResponse: T? = response.body()
+                    if (rawResponse != null) {
+                        emit(Result.Success(rawResponse))
+                    } else {
+                        emit(Result.Error(ApiError.ServiceError(message = response.message())))
+                    }
+                } else {
+                    emit(Result.Error(errorParser(response.code())))
+                }
+
+            } catch (e: Exception) {
+                Timber.e(e)
+                emit(Result.Error(ApiError.Unavailable))
+            }
+        }.catch {
+            emit(Result.Error(ApiError.Unavailable))
+        }
+    }
 }
