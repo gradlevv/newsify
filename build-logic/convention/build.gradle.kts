ec4bc0dab425b66816aa2626509b758d0bf94b29
Javad <josmithth2828@gmail.com>
2025-03-07 15:24:33 +0330

    wip --- create build-logic moduule to organize gradle plugins

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    `kotlin-dsl`
+}
+
+dependencies {
+    implementation(libs.android.gradlePlugin)
+    implementation(libs.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        create("androidApplication") {
+            id = "newsify.android.application"
+            implementationClass = "NewsifyAndroidApplicationPlugin"
+            description = "Android Application plugin for Newsify"
+        }
+        create("androidLibrary") {
+            id = "newsify.android.library"
+            implementationClass = "NewsifyAndroidLibraryPlugin"
+            description = "Android Library plugin for Newsify"
+        }
+    }
+} 
