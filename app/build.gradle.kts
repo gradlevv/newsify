b541e8271d9c84c649c75881439f0f22f510c74c
Javad <j.sadatii@gmail.com>
2025-05-16 12:13:43 +0330

    add Compose compiler dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.navigation.safe.args)
+    alias(libs.plugins.compose.compiler)
     id("com.google.dagger.hilt.android")
 }
 
