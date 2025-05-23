e40a05300f83faa5700ac16c3c22dd5937d08ac4
Javad <j.sadatii@gmail.com>
2025-05-23 17:14:49 +0330

    update NewsifyNavigationBar

--- app/src/main/java/com/gradlevv/newsify/navigation/Navigation.kt
+++ app/src/main/java/com/gradlevv/newsify/navigation/Navigation.kt
@@ -3,38 +3,71 @@ package com.gradlevv.newsify.navigation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.data.SourceLocation
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hasRoute
+import com.gradlevv.list.ui.NewsListDestination
+import com.gradlevv.newsify.ui.R
+import com.gradlevv.search.ui.SearchNewsDestination
+import com.gradlevv.setting.ui.SettingDestination
+import com.gradlevv.sources.ui.NewsSourcesDestination
 
 
 @Composable
-fun  NewsifyNavigationBar(
-    destinations: List<TopLevelScreen>,
-    onNavigationSelected: (TopLevelScreen) -> Unit,
-    modifier: Modifier
+fun NewsifyNavigationBar(
+    hierarchy: Sequence<NavDestination>?,
+    onNavigateToHomeClick: () -> Unit,
+    onNavigateToSearchClick: () -> Unit,
+    onNavigateToSourcesClick: () -> Unit,
+    onNavigateToSettingClick: () -> Unit,
 ) {
-    NavigationBar(modifier = modifier) {
+    NavigationBar {
 
-        destinations.forEach { current ->
-            NavigationBarItem(
-                selected = false,
-                onClick = { onNavigationSelected(current) },
-                icon = {
-                    Icon(
-                        imageVector = current.selectedIcon,
-                        contentDescription = ""
-                    )
-                },
-                label = {
-                    Text(
-                        text = stringResource(current.titleTextId)
-                    )
-                }
+        NavigationBarItem(
+            selected = hierarchy?.any { it.hasRoute(NewsListDestination::class) } == true,
+            onClick = onNavigateToHomeClick,
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_home_fill),
+                    contentDescription = ""
+                )
+            }
+        )
 
-            )
-        }
+        NavigationBarItem(
+            selected = hierarchy?.any { it.hasRoute(SearchNewsDestination::class) } == true,
+            onClick = onNavigateToSearchClick,
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_search_fill),
+                    contentDescription = ""
+                )
+            }
+        )
+
+        NavigationBarItem(
+            selected = hierarchy?.any { it.hasRoute(NewsSourcesDestination::class) } == true,
+            onClick = onNavigateToSourcesClick,
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_global_fill),
+                    contentDescription = ""
+                )
+            }
+        )
+
+        NavigationBarItem(
+            selected = hierarchy?.any { it.hasRoute(SettingDestination::class) } == true,
+            onClick = onNavigateToSettingClick,
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_settings_fill),
+                    contentDescription = ""
+                )
+            }
+        )
 
     }
 }
