a1c62a04a2d251effdc15d66e100f8860a3fb43d
Javad <j.sadatii@gmail.com>
2025-05-29 19:29:16 +0330

    add new colors

--- ui/src/main/java/com/gradlevv/ui/theme/Color.kt
+++ ui/src/main/java/com/gradlevv/ui/theme/Color.kt
@@ -3,3 +3,5 @@ package com.gradlevv.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val ColorStatusBar = Color(0xFFF7F7FC)
+val ColorOnBackground70 = Color(0xFF9FA5C4)
+val ColorOnBackground100 = Color(0xFF313A66)
