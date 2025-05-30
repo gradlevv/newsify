928186fff6778b1651017854133ea630569ea531
Javad <j.sadatii@gmail.com>
2025-05-30 09:43:29 +0330

    create TypeItemListComponent component

--- news/list/src/main/java/com/gradlevv/list/ui/NewsListScreen.kt
+++ news/list/src/main/java/com/gradlevv/list/ui/NewsListScreen.kt
@@ -1,17 +1,80 @@
 package com.gradlevv.list.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.gradlevv.list.domain.CategoryItem
+import com.gradlevv.newsify.news.list.R
+import com.gradlevv.ui.theme.ColorOnBackground100
+import com.gradlevv.ui.theme.ColorOnBackground70
 
 
 @Composable
-fun NewsListScreen() {
-    Text("NewsListScreen")
+fun NewsListScreen(viewModel: NewsListViewModel = hiltViewModel()) {
+
+    val data by viewModel.categoryList.collectAsState()
+
+    TypeItemListComponent(data = data)
 }
 
 @Composable
-fun TypeItemComponent() {
-    Column {
+fun TypeItemListComponent(
+    modifier: Modifier = Modifier,
+    data: List<CategoryItem>
+) {
+    Column(modifier = modifier) {
+        Text(
+            modifier = Modifier.padding(start = 12.dp),
+            text = stringResource(R.string.news_list_categories_title),
+            color = ColorOnBackground100
+        )
+        LazyRow(
+            modifier = Modifier.padding(top = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(start = 8.dp, end = 8.dp)
+        ) {
+            items(data, key = { it.type }) { item ->
+                TypeItemComponent(item = item)
+            }
+        }
+    }
+
+}
+
+@Composable
+fun TypeItemComponent(
+    modifier: Modifier = Modifier,
+    item: CategoryItem
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(item.icon),
+            contentDescription = "",
+            modifier = Modifier
+                .padding(bottom = 4.dp)
+                .size(95.dp)
+                .clip(RoundedCornerShape(20.dp)),
+        )
+        Text(text = stringResource(item.categoryName), color = ColorOnBackground70)
     }
 }
--- ui/src/main/java/com/gradlevv/ui/theme/Color.kt
+++ ui/src/main/java/com/gradlevv/ui/theme/Color.kt
@@ -3,5 +3,6 @@ package com.gradlevv.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val ColorStatusBar = Color(0xFFF7F7FC)
+val ColorPrimary = Color(0xFF118DFF)
 val ColorOnBackground70 = Color(0xFF9FA5C4)
 val ColorOnBackground100 = Color(0xFF313A66)
