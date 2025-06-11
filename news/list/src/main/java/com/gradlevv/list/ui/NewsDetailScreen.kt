b5a80a7c426c0c7c452ae4129f7f731f9058e268
Javad Sadati <j.sadatii@gmail.com>
2025-06-11 14:11:37 +0330

    wip --- implement NewsDetailScreen

--- news/list/src/main/java/com/gradlevv/list/ui/NewsDetailScreen.kt
+++ news/list/src/main/java/com/gradlevv/list/ui/NewsDetailScreen.kt
@@ -1,23 +1,21 @@
 package com.gradlevv.list.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.gradlevv.newsify.news.list.R
-import com.gradlevv.ui.component.FullScreenCentered
+import com.gradlevv.list.ui.state.TopHeadLinesState
 import com.gradlevv.ui.theme.ColorOnBackground100
-import com.gradlevv.ui.theme.ColorPrimary
 
 @Composable
 fun NewsDetailScreen(navController: NavHostController) {
@@ -25,47 +23,60 @@ fun NewsDetailScreen(navController: NavHostController) {
     val parentEntry = remember(navController) { navController.getBackStackEntry(NewsGraph.route) }
 
     val viewModel: NewsListViewModel = hiltViewModel(parentEntry)
-    val topHeadLines by viewModel.topHeadLinesList.collectAsState()
+    val uiState by viewModel.topHeadLinesList.collectAsState()
 
 
-    when {
-
-        topHeadLines.isLoading -> {
-
-            FullScreenCentered {
-                CircularProgressIndicator(
-                    modifier = Modifier
-                        .size(72.dp)
-                        .align(Alignment.Center),
-                    color = ColorOnBackground100,
-                    trackColor = ColorPrimary,
-                )
-            }
-
-        }
-
-        topHeadLines.isError -> {
-
-            FullScreenCentered {
-                Text(
-                    text = stringResource(
-                        R.string.news_list_something_gets_wrong
-                    ),
-                    modifier = Modifier.align(Alignment.Center)
-                )
-            }
-        }
-
-        topHeadLines.items.isNotEmpty() -> {
-            Column {
-                TopNewsListComponent(
-                    data = topHeadLines,
-                    onItemClick = {}
-                )
-            }
-        }
-    }
+    NewsCategoryComponent(
+        uiState = uiState,
+        onDetailClick = {}
+    )
 
 }
 
+
+@Composable
+fun NewsCategoryComponent(
+    uiState: TopHeadLinesState,
+    onDetailClick: () -> Unit,
+    loadingContent: @Composable () -> Unit = {
+        LoadingComponent()
+    },
+    errorContent: @Composable () -> Unit = {
+        ErrorComponent()
+    },
+    content: @Composable () -> Unit = {
+        SelectedNewsComponent(
+            uiState = uiState,
+            onDetailClick = onDetailClick
+        )
+    }
+) {
+    when {
+        uiState.isLoading -> loadingContent()
+        uiState.isError -> errorContent()
+        else -> content()
+    }
+}
+
+@Composable
+fun SelectedNewsComponent(
+    uiState: TopHeadLinesState,
+    onDetailClick: () -> Unit,
+) {
+    TopNewsListComponent(
+        data = uiState,
+        onItemClick = onDetailClick
+    ) {
+        Text(
+            modifier = Modifier.padding(
+                start = 16.dp,
+            ),
+            text = stringResource(uiState.categoryName),
+            color = ColorOnBackground100,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Start,
+        )
+    }
+}
 
