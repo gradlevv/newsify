35075f1fe1749576b261066d0f247c2d5e441f25
Javad <j.sadatii@gmail.com>
2025-06-13 18:51:17 +0330

    update Error models

--- core/src/main/java/com/gradlevv/core/data/model/Error.kt
+++ core/src/main/java/com/gradlevv/core/data/model/Error.kt
@@ -3,6 +3,7 @@ package com.gradlevv.core.data.model
 sealed class ApiError {
 
     object Unavailable: ApiError()
+    object NullError: ApiError()
     object RateLimitError: ApiError()
     data class ServiceError(val message: String = "") : ApiError()
 
