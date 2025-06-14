60f933b001e25b8a32d3a1e012a3c959c8226b3e
Javad <j.sadatii@gmail.com>
2025-06-14 13:26:53 +0330

    combine di module classes into each other

--- news/list/src/main/java/com/gradlevv/list/di/BinderModule.kt
+++ news/list/src/main/java/com/gradlevv/list/di/BinderModule.kt
@@ -1,11 +1,14 @@
 package com.gradlevv.list.di
 
 import com.gradlevv.list.data.source.NewsListRepositoryImpl
+import com.gradlevv.list.data.source.NewsListService
 import com.gradlevv.list.domain.NewsListRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import retrofit2.Retrofit
 
 @InstallIn(ViewModelComponent::class)
 @Module
@@ -13,5 +16,12 @@ abstract class BinderModule {
 
     @Binds
     abstract fun bindNewsListRepository(newsListRepositoryImpl: NewsListRepositoryImpl) : NewsListRepository
+
+    companion object {
+        @Provides
+        fun provideNewsListService(retrofit: Retrofit): NewsListService {
+            return retrofit.create(NewsListService::class.java)
+        }
+    }
 }
 
