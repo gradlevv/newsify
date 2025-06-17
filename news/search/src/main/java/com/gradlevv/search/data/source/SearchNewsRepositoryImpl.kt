583069cbcf14ce5307bb468c27bcc279f3d4109a
Javad <j.sadatii@gmail.com>
2025-06-17 14:37:24 +0330

    remove useless extend keyword

--- news/search/src/main/java/com/gradlevv/search/data/source/SearchNewsRepositoryImpl.kt
+++ news/search/src/main/java/com/gradlevv/search/data/source/SearchNewsRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.gradlevv.search.data.source
 import com.gradlevv.core.data.model.ApiError
 import com.gradlevv.core.data.model.Result
 import com.gradlevv.core.data.model.map
-import com.gradlevv.core.data.network.ResponseHandler
 import com.gradlevv.core.data.network.safeApiCall
 import com.gradlevv.search.data.model.toDomain
 import com.gradlevv.search.domain.SearchNewsItem
@@ -13,7 +12,7 @@ import javax.inject.Inject
 
 class SearchNewsRepositoryImpl @Inject constructor(
     private val searchNewsService: SearchNewsService,
-) : ResponseHandler(), SearchNewsRepository {
+) : SearchNewsRepository {
 
     override suspend fun searchNews(params: Params): Result<List<SearchNewsItem>> {
         return safeApiCall {
