89a1dd9cd54ffd35324c7a0be41189085fc86545
Javad <j.sadatii@gmail.com>
2025-06-17 18:24:51 +0330

    remove useless extensions

--- ui/src/main/java/com/gradlevv/ui/utils/ViewExtentions.kt
+++ ui/src/main/java/com/gradlevv/ui/utils/ViewExtentions.kt
@@ -1,8 +1,5 @@
 package com.gradlevv.ui.utils
 
-import android.content.Context
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.navigation.NavOptions
@@ -20,25 +17,6 @@ fun ImageView.loadImage(url: String?){
         .load(url)
         .placeholder(R.drawable.ic_place_holder)
         .into(this)
-}
-
-fun View.closeKeyboard() {
-    try {
-        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(windowToken, 0)
-    } catch (exc: Exception) {
-        exc.printStackTrace()
-    }
-}
-
-fun View.openKeyboard(): Boolean {
-    try {
-        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        return imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
-    } catch (exc: Exception) {
-        exc.printStackTrace()
-    }
-    return false
 }
 
 val navOptions = NavOptions.Builder()
--- ui/src/main/java/com/gradlevv/ui/utils/ViewGroupExtensions.kt
+++ ui/src/main/java/com/gradlevv/ui/utils/ViewGroupExtensions.kt
@@ -1,82 +1,10 @@
 package com.gradlevv.ui.utils
 
-import android.content.Context
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.LinearLayout
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.gradlevv.core.util.dp
 
-var ViewGroup.MarginLayoutParams.horizontalMargin: Int
-    get() = 0
-    set(value) {
-        rightMargin = value
-        leftMargin = value
-    }
-
-var ViewGroup.MarginLayoutParams.verticalMargin: Int
-    get() = 0
-    set(value) {
-        topMargin = value
-        bottomMargin = value
-    }
-
-fun ConstraintLayout.customWidthAndHeight(wInDp: Int, hInDp: Int): ConstraintLayout.LayoutParams {
-    return ConstraintLayout.LayoutParams(wInDp.dp(), hInDp.dp())
-}
-
-fun ConstraintLayout.wrapWidthCustomHeight(hInDp: Int): ConstraintLayout.LayoutParams {
-    return ConstraintLayout.LayoutParams(
-        ConstraintLayout.LayoutParams.WRAP_CONTENT,
-        hInDp.dp()
-    )
-}
-
-fun ConstraintLayout.matchWidthWrapHeight(): ConstraintLayout.LayoutParams {
-    return ConstraintLayout.LayoutParams(
-        ConstraintLayout.LayoutParams.MATCH_PARENT,
-        ConstraintLayout.LayoutParams.WRAP_CONTENT
-    )
-}
-
-fun ConstraintLayout.wrapWidthWrapHeight(): ConstraintLayout.LayoutParams {
-    return ConstraintLayout.LayoutParams(
-        ConstraintLayout.LayoutParams.WRAP_CONTENT,
-        ConstraintLayout.LayoutParams.WRAP_CONTENT
-    )
-}
-
-fun ConstraintLayout.matchConstraintWrapHeight(): ConstraintLayout.LayoutParams {
-    return ConstraintLayout.LayoutParams(
-        ConstraintLayout.LayoutParams.MATCH_CONSTRAINT,
-        ConstraintLayout.LayoutParams.WRAP_CONTENT
-    )
-}
-
-fun ConstraintLayout.matchConstraintWidthAndHeight(): ConstraintLayout.LayoutParams {
-    return ConstraintLayout.LayoutParams(
-        ConstraintLayout.LayoutParams.MATCH_CONSTRAINT,
-        ConstraintLayout.LayoutParams.MATCH_CONSTRAINT
-    )
-}
-
-fun ViewGroup.customWidthAndHeight(wInDp: Int, hInDp: Int): ViewGroup.LayoutParams {
-    return ViewGroup.LayoutParams(wInDp.dp(), hInDp.dp())
-}
-
-fun ViewGroup.wrapWidthAndHeight(): ViewGroup.LayoutParams {
-    return ViewGroup.LayoutParams(
-        ViewGroup.LayoutParams.WRAP_CONTENT,
-        ViewGroup.LayoutParams.WRAP_CONTENT
-    )
-}
-
-fun ViewGroup.matchWidthAndWrapHeight(): ViewGroup.LayoutParams {
-    return ViewGroup.LayoutParams(
-        ViewGroup.LayoutParams.MATCH_PARENT,
-        ViewGroup.LayoutParams.WRAP_CONTENT
-    )
-}
 
 fun ViewGroup.matchWidthCustomHeight(hInDp: Int): ViewGroup.LayoutParams {
     return ViewGroup.LayoutParams(
@@ -85,12 +13,6 @@ fun ViewGroup.matchWidthCustomHeight(hInDp: Int): ViewGroup.LayoutParams {
     )
 }
 
-fun ViewGroup.matchWidthAndHeight(): ViewGroup.LayoutParams {
-    return ViewGroup.LayoutParams(
-        ViewGroup.LayoutParams.MATCH_PARENT,
-        ViewGroup.LayoutParams.MATCH_PARENT
-    )
-}
 
 fun LinearLayout.customWidthAndHeight(
     wInDp: Int,
@@ -113,22 +35,6 @@ fun LinearLayout.customWidthAndWrapHeight(
     ).apply(initLayout)
 }
 
-fun LinearLayout.wrapWidthAndHeight(initLayout: LinearLayout.LayoutParams.() -> Unit = {}): LinearLayout.LayoutParams {
-    return LinearLayout.LayoutParams(
-        LinearLayout.LayoutParams.WRAP_CONTENT,
-        LinearLayout.LayoutParams.WRAP_CONTENT
-    ).apply(initLayout)
-}
-
-fun LinearLayout.wrapWidthCustomHeight(
-    hInDp: Int,
-    initLayout: LinearLayout.LayoutParams.() -> Unit = {}
-): LinearLayout.LayoutParams {
-    return LinearLayout.LayoutParams(
-        LinearLayout.LayoutParams.WRAP_CONTENT,
-        hInDp.dp()
-    ).apply(initLayout)
-}
 
 fun LinearLayout.matchWidthAndCustomHeight(
     hInDp: Int,
@@ -192,10 +98,6 @@ fun FrameLayout.wrapWidthAndHeight(initLayout: FrameLayout.LayoutParams.() -> Un
         FrameLayout.LayoutParams.WRAP_CONTENT,
         FrameLayout.LayoutParams.WRAP_CONTENT
     ).apply(initLayout)
-}
-
-fun Context.frameLayout(initLayout: FrameLayout.() -> Unit = {}): FrameLayout {
-    return FrameLayout(this).apply(initLayout)
 }
 
 
