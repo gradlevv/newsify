f01cf146a5fdaf4513381a8ed3e33aee96b34cde
Javad <j.sadatii@gmail.com>
2025-06-17 14:32:42 +0330

    refactor di layer

--- news/search/src/main/java/com/gradlevv/search/di/BinderModule.kt
+++ news/search/src/main/java/com/gradlevv/search/di/BinderModule.kt
@@ -1,18 +1,27 @@
 package com.gradlevv.search.di
 
 import com.gradlevv.search.data.source.SearchNewsRepositoryImpl
+import com.gradlevv.search.data.source.SearchNewsService
 import com.gradlevv.search.domain.SearchNewsRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import retrofit2.Retrofit
 
 @InstallIn(ViewModelComponent::class)
 @Module
 abstract class BinderModule {
 
     @Binds
-    abstract fun bindSearchNewsRepository(searchNewsRepositoryImpl: SearchNewsRepositoryImpl): SearchNewsRepository
+    abstract fun bindSearchNewsRepository(impl: SearchNewsRepositoryImpl): SearchNewsRepository
 
+    companion object {
+        @Provides
+        fun provideSearchNewsService(retrofit: Retrofit): SearchNewsService {
+            return retrofit.create(SearchNewsService::class.java)
+        }
+    }
 }
 
