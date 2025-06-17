b0cd1a47ac7f7e5763aa2eaa267b3cb0de7b5544
Javad <j.sadatii@gmail.com>
2025-06-17 18:15:34 +0330

    remove useless extension function

--- core/src/main/java/com/gradlevv/core/data/model/Result.kt
+++ core/src/main/java/com/gradlevv/core/data/model/Result.kt
@@ -1,18 +1,9 @@
 package com.gradlevv.core.data.model
 
-import com.gradlevv.core.mapper.BaseDataMapper
-
 
 sealed class Result<out T> {
     data class Success<T>(val data: T?) : Result<T>()
     data class Error<T>(val error: ApiError?) : Result<T>()
-}
-
-fun <From, To> Result<From>.mapTo(mapper: BaseDataMapper<From, To>): Result<To> {
-    return when (val resource = this) {
-        is Result.Error -> Result.Error(resource.error)
-        is Result.Success -> Result.Success(resource.data?.let { mapper.mapTo(it) })
-    }
 }
 
 inline fun <From, To> Result<From>.map(transform: (From) -> To): Result<To> {
