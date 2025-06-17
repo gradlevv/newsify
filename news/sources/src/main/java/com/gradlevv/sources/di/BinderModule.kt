bbcbf0ab9ad421e7bfe9dc0b380444a1a51719cd
Javad <j.sadatii@gmail.com>
2025-06-17 18:13:29 +0330

    refactor di layer

--- news/sources/src/main/java/com/gradlevv/sources/di/BinderModule.kt
+++ news/sources/src/main/java/com/gradlevv/sources/di/BinderModule.kt
@@ -1,17 +1,27 @@
 package com.gradlevv.sources.di
 
 import com.gradlevv.sources.data.source.SourcesRepositoryImpl
+import com.gradlevv.sources.data.source.SourcesService
 import com.gradlevv.sources.domain.repository.SourcesRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import retrofit2.Retrofit
 
 @InstallIn(ViewModelComponent::class)
 @Module
 abstract class BinderModule {
 
     @Binds
-    abstract fun bindNewsSourceRepository(sourcesRepositoryImpl: SourcesRepositoryImpl): SourcesRepository
+    abstract fun bindNewsSourceRepository(impl: SourcesRepositoryImpl): SourcesRepository
+
+    companion object {
+        @Provides
+        fun provideSourcesService(retrofit: Retrofit): SourcesService {
+            return retrofit.create(SourcesService::class.java)
+        }
+    }
 }
 
