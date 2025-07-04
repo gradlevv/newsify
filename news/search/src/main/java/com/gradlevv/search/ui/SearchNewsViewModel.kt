1ed1e247fe50aaeaafdcc1bb1d9d99690e8339d3
Javad <j.sadatii@gmail.com>
2025-07-04 09:44:38 +0330

    remove useless fragment

--- news/search/src/main/java/com/gradlevv/search/ui/SearchNewsViewModel.kt
+++ news/search/src/main/java/com/gradlevv/search/ui/SearchNewsViewModel.kt
@@ -1,8 +1,8 @@
 package com.gradlevv.search.ui
 
 import android.util.Log
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.gradlevv.core.base.BaseViewModel
 import com.gradlevv.core.data.model.Result
 import com.gradlevv.core.util.Constants.DATE_FORMAT
 import com.gradlevv.core.util.Constants.SORT_BY
@@ -27,7 +27,7 @@ import javax.inject.Inject
 @HiltViewModel
 class SearchNewsViewModel @Inject constructor(
     private val searchNewsUseCase: SearchNewsUseCase
-) : BaseViewModel() {
+) : ViewModel() {
 
     private val _searchNewsList = MutableStateFlow(SearchNewsState.Empty)
     val searchNewsList = _searchNewsList.asStateFlow()
@@ -70,7 +70,6 @@ class SearchNewsViewModel @Inject constructor(
 
                             is Result.Error -> {
                                 _searchNewsList.value = SearchNewsState(isError = true)
-                                errorMessage.value = result.error
                             }
                         }
                     }
@@ -89,7 +88,7 @@ class SearchNewsViewModel @Inject constructor(
 
     fun navigateToDetailFragment(item: SearchNewsItem) {
         _newsDetailItem.value = item
-        navigate(R.string.search_news_detail_fragment, navOptions)
+//        navigate(R.string.search_news_detail_fragment, navOptions)
     }
 
     companion object {
